ff09cf1e75683409d1a89b5cea0ed6e72db34d53
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-18 14:51:21 +0530

    fix

--- modules/terraform-aws-rds/main.tf
+++ modules/terraform-aws-rds/main.tf
@@ -44,6 +44,16 @@ resource "aws_rds_cluster" "default" {
   db_subnet_group_name   = var.private_subnet_group_name
   skip_final_snapshot    = true
 }
+resource "aws_rds_cluster_instance" "writer" {
+  identifier              = "aurora-cluster-test-instance-1"
+  cluster_identifier      = aws_rds_cluster.default.id
+  instance_class          = "db.t3.medium"
+  engine                  = aws_rds_cluster.default.engine
+  engine_version          = aws_rds_cluster.default.engine_version
+  publicly_accessible     = false
+  db_subnet_group_name    = var.private_subnet_group_name
+  availability_zone       = data.aws_availability_zones.az.names[0]
+}
 
 
 
