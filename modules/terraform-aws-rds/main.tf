02c7999209f5b5a8970534aab74bcecf998725b7
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-18 10:02:13 +0530

    fix

--- modules/terraform-aws-rds/main.tf
+++ modules/terraform-aws-rds/main.tf
@@ -1,21 +1,15 @@
-# resource "aws_db_instance" "default" {
-#   allocated_storage       = 10
-#   storage_type            = "gp2"
-#   engine                  = "mysql"
-#   engine_version          = "5.7"
-#   instance_class          = "db.t3.micro"
-#   identifier              = "mydb"
-#   username                = "dbuser"
-#   password                = "dbpassword"
-#   availability_zone       = "ap-south-1a"
-#   backup_retention_period = 7
+data "aws_secretsmanager_secret" "db" {
+  arn = "arn:aws:secretsmanager:ap-southeast-1:495599733393:secret:MyDBSecret-jBG0uj"
+}
 
-#   #vpc_security_group_ids = [aws_security_group.rds_sg.id]
-#   vpc_security_group_ids = var.vpc_security_group_id
-#   db_subnet_group_name   = var.private_subnet_group_name
-#   publicly_accessible    = false
-#   skip_final_snapshot    = true
-# }
+data "aws_secretsmanager_secret_version" "db_version" {
+  secret_id = data.aws_secretsmanager_secret.db.id
+}
+
+locals {
+  db_secret = jsondecode(data.aws_secretsmanager_secret_version.db_version.secret_string)
+}
+
 
 
 
@@ -26,8 +20,8 @@ resource "aws_rds_cluster" "default" {
   engine_version          = "5.7.mysql_aurora.2.03.2"
   availability_zones      = ["ap-south-1a"]
   database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "bar"
+  master_username         = local.db_secret.username
+  master_password         = local.db_secret.password
   backup_retention_period = 5
   vpc_security_group_ids = var.vpc_security_group_id
   db_subnet_group_name   = var.private_subnet_group_name
@@ -36,28 +30,14 @@ resource "aws_rds_cluster" "default" {
 
 
 
-resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "aurora-db-credentials"
-}
+# resource "aws_secretsmanager_secret" "db_credentials" {
+#   name = "aurora-db-credentials"
+# }
 
-resource "aws_secretsmanager_secret_version" "db_credentials_version" {
-  secret_id     = aws_secretsmanager_secret.db_credentials.id
-  secret_string = jsonencode({
-    username = "prakash"
-    password = "prakash-secret"
-  })
-}
-
-# resource "aws_db_instance" "read_replica" {
-#   #   replicate_source_db = aws_db_instance.default.id
-#   replicate_source_db     = aws_db_instance.default.arn
-#   instance_class          = "db.t3.micro"
-#   publicly_accessible     = false
-#   availability_zone       = "ap-south-1b"
-#   identifier              = "mydb-replica"
-#   db_subnet_group_name    = var.private_subnet_group_name
-#   vpc_security_group_ids  = var.vpc_security_group_id
-#   depends_on              = [aws_db_instance.default]
-#   backup_retention_period = 7
-#   skip_final_snapshot     = true
+# resource "aws_secretsmanager_secret_version" "db_credentials_version" {
+#   secret_id     = aws_secretsmanager_secret.db_credentials.id
+#   secret_string = jsonencode({
+#     username = "prakash"
+#     password = "prakash-secret"
+#   })
 # }
