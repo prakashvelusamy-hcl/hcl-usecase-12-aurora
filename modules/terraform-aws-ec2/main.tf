de8a007d64d3f71dd4b8982e3f9d694b6f98cef1
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-19 09:33:27 +0530

    install mysql client

--- modules/terraform-aws-ec2/main.tf
+++ modules/terraform-aws-ec2/main.tf
@@ -58,6 +58,9 @@ resource "aws_instance" "public_instances" {
                 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 sudo unzip awscliv2.zip
                 sudo ./aws/install
+                sudo apt update
+                sudo apt install -y mysql-client
+
                 EOF
 
   tags = {
